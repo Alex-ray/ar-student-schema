ac75a73fc713c5a5ce4f4ddf251b394becc44aa2
Apprentice <apprentice@devbootcamp.com>
2013-03-28 16:00:36 -0700

    added and populated teachers table

---
+++ app/models/teacher.rb
@@ -0,0 +1,8 @@
+require_relative '../../db/config'
+
+class Teacher < ActiveRecord::Base
+  # validates :email, :uniqueness => true
+  # validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :on => :create
+  # validates_format_of :phone, :with => /^[\\(]{0,1}([0-9]){3}[\\)]{0,1}[ ]?([^0-1]){1}([0-9]){2}[ ]?[-]?[ ]?([0-9]){4}[ ]*((x){0,1}([0-9]){1,5}){0,1}$/, :on => :create
+
+end
--- spec/migrate_create_table_spec.rb
+++ spec/migrate_create_table_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require_relative '../db/config'
 
 
-describe "create table with correct schema" do
+describe "create students table with correct schema" do
   before(:all) do
     raise RuntimeError, "be sure to run 'rake db:migrate' before running these specs" unless ActiveRecord::Migrator.current_version > 0
   end
@@ -19,6 +19,27 @@ describe "create table with correct schema" do
     }
 
     ActiveRecord::Base.connection.columns(:students).each do |col|
+      expected[col.type].include?(col.name).should be_true
+    end
+  end
+end
+
+describe "create teachers table with correct schema" do
+  before(:all) do
+    raise RuntimeError, "be sure to run 'rake db:migrate' before running these specs" unless ActiveRecord::Migrator.current_version > 0
+  end
+
+  it "should have a Teachers table" do
+    ActiveRecord::Base.connection.table_exists?(:teachers).should be_true
+  end
+
+  it "should have the right columns and types" do
+    expected = {
+      :integer => ["id"],
+      :string => ["first_name", "last_name", "email", "phone"]
+    }
+
+    ActiveRecord::Base.connection.columns(:teachers).each do |col|
       expected[col.type].include?(col.name).should be_true
     end
   end
