2c1e7eb408f7ec7cbdaf8cadb0e9a32d6b8e2e71
Apprentice <apprentice@devbootcamp.com>
2013-03-28 12:18:46 -0700

    updated phone validation regexp

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,7 +4,7 @@ class Student < ActiveRecord::Base
   validates :email, :uniqueness => true
   validates :age, :numericality => { :greater_than => 3 }
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :on => :create
-  validates_format_of :phone, :with => /.{0,}\d.{0,}\d.{0,}\d.{0,}\d.{0,}\d.{0,}\d.{0,}\d.{0,}\d.{0,}\d.{0,}\d.*/, :on => :create
+  validates_format_of :phone, :with => /^[\\(]{0,1}([0-9]){3}[\\)]{0,1}[ ]?([^0-1]){1}([0-9]){2}[ ]?[-]?[ ]?([0-9]){4}[ ]*((x){0,1}([0-9]){1,5}){0,1}$/, :on => :create
 
   def name
     "#{first_name} #{last_name}"
