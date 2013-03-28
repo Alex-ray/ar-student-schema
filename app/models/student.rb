ab82f21fa517f43dde5af5f94e14e790f7364f62
Apprentice <apprentice@devbootcamp.com>
2013-03-28 12:14:05 -0700

    All tests pass

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,17 @@
 require_relative '../../db/config'
 
-# implement your Student model here
+class Student < ActiveRecord::Base 
+  validates :email, :uniqueness => true
+  validates :age, :numericality => { :greater_than => 3 }
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :on => :create
+  validates_format_of :phone, :with => /.{0,}\d.{0,}\d.{0,}\d.{0,}\d.{0,}\d.{0,}\d.{0,}\d.{0,}\d.{0,}\d.{0,}\d.*/, :on => :create
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def age
+    now = Time.now
+    now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
+  end
+end
